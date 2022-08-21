4a8b2d8804e66feaf05dfab4472c7f5c0d6161c7
Iván Litteri <ilitteri@fi.uba.ar>
2022-08-21 16:34:59 -0300

    Add web server documentation

--- src/web_server.rs
+++ src/web_server.rs
@@ -13,6 +13,7 @@ struct Requester {
     voice_channel: u64,
 }
 
+/// An enum of error handlers for the server.
 #[derive(Debug)]
 enum ServerError {
     Request(String),
@@ -20,16 +21,20 @@ enum ServerError {
 
 impl reject::Reject for ServerError {}
 
+/// A trait to unwrap a `Result` or `Reject`.
 pub trait OrReject<T> {
+    /// Returns the result if it is successful, otherwise returns a rejection.
     fn or_reject(self) -> Result<T, Rejection>;
 }
 
 impl<T> OrReject<T> for anyhow::Result<T> {
+    /// Returns the result if it is successful, otherwise returns a rejection.
     fn or_reject(self) -> Result<T, Rejection> {
         self.map_err(|e| reject::custom(ServerError::Request(e.to_string())))
     }
 }
 
+/// An enum of requests that the `HelpQueue` struct processes.
 #[derive(Debug)]
 pub enum HelpQueueRequest {
     Request((u16, u64)),
@@ -37,6 +42,7 @@ pub enum HelpQueueRequest {
     Dismiss(u16)
 }
 
+/// Arguments that serve as config for the server.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct ServerArguments {
@@ -65,10 +71,12 @@ impl Default for ServerArguments {
     }
 }
 
+/// A middleware to include the given item in the handler.
 fn with<T: Clone + Send>(item: T) -> impl Filter<Extract = (T,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || item.clone())
 }
 
+/// A server for the help queue.
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct WebServer {
@@ -77,13 +85,16 @@ pub struct WebServer {
 }
 
 impl WebServer {
+    /// Initializes a new instance of the server.
     pub fn start(help_queue: Arc<HelpQueue>, args: ServerArguments) -> Result<Self> {
+        // Initialize a runtime.
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .thread_stack_size(8 * 1024 * 1024)
             .build()?;
 
         let serve_args = args.clone();
+        // Initialize the server.
         runtime.block_on(async move {
             let _ = Self::start_server(help_queue.clone(), serve_args).await;
         });
@@ -96,8 +107,10 @@ impl WebServer {
     }
 
     fn start_server(help_queue: Arc<HelpQueue>, args: ServerArguments) -> JoinHandle<()> {
+        // Prepare the list of routes.
         let routes = Self::routes(help_queue);
         tokio::spawn(async move {
+            // Start the server.
             println!("\n🌐 Server is running at {}:{}\n", args.domain, args.port);
             warp::serve(routes).run(([127, 0, 0, 1], args.port)).await;
         })
@@ -106,6 +119,7 @@ impl WebServer {
     fn routes(
         help_queue: Arc<HelpQueue>,
     ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+        // GET /api/discord/v1/next
         let next = warp::get()
             .and(warp::path!("api" / "discord" / "v1" / "next"))
             .and(warp::body::content_length_limit(64))
@@ -113,6 +127,7 @@ impl WebServer {
             .and(with(help_queue.clone()))
             .and_then(Self::next);
 
+        // POST /api/discord/v1/dismiss_help
         let dismiss_help = warp::get()
             .and(warp::path!("api" / "discord" / "v1" / "dismiss_help"))
             .and(warp::body::content_length_limit(2))
@@ -120,6 +135,7 @@ impl WebServer {
             .and(with(help_queue.clone()))
             .and_then(Self::dismiss_help);
 
+        // POST /api/discord/v1/enqueue_help
         let request_help = warp::post()
             .and(warp::path!("api" / "discord" / "v1" / "enqueue_help"))
             .and(warp::body::content_length_limit(10 * 1024 * 1024))
@@ -127,16 +143,19 @@ impl WebServer {
             .and(with(help_queue.clone()))
             .and_then(Self::request_help);
 
+        // PATCH /api/discord/v1/clear_help_queue
         let clear_queue = warp::patch()
             .and(warp::path!("api" / "discord" / "v1" / "clear_help_queue"))
             .and(with(help_queue.clone()))
             .and_then(Self::clear_help_queue);
 
+        // GET /api/discord/v1/help_queue
         let get_help_queue = warp::get()
             .and(warp::path!("api" / "discord" / "v1" / "help_queue"))
             .and(with(help_queue))
             .and_then(Self::get_help_queue);
         
+        // Return the list of routes.
         next
             .or(dismiss_help)
             .or(request_help)
@@ -144,26 +163,31 @@ impl WebServer {
             .or(get_help_queue)
     }
 
+    /// Returns the next group in the help queue.
     async fn next(helper: String, help_queue: Arc<HelpQueue>) -> Result<impl Reply, Rejection> {
         let (group, voice_channel) = help_queue.next(helper).await.or_reject()?;
         Ok(reply::with_status(reply::json(&serde_json::json!({"group": group, "voice_channel": voice_channel})), StatusCode::OK))
     }
 
+    /// Removes the dismisser from the help queue.
     async fn dismiss_help(dismisser: u16, help_queue: Arc<HelpQueue>) -> Result<impl Reply, Rejection> {
         let (group, voice_channel) = help_queue.dismiss(dismisser).await.or_reject()?;
         Ok(reply::with_status(reply::json(&serde_json::json!({"group": group, "voice_channel": voice_channel})), StatusCode::OK))
     }
 
+    /// Pushes a requester to the help queue.
     async fn request_help(requester: Requester, help_queue: Arc<HelpQueue>) -> Result<impl Reply, Rejection> {
         help_queue.enqueue(requester.group, requester.voice_channel).await.or_reject()?;
         Ok(reply::with_status(reply::reply(), StatusCode::OK))
     }
 
+    /// Clears the help queue.
     async fn clear_help_queue(help_queue: Arc<HelpQueue>) -> Result<impl Reply, Rejection> {
         help_queue.clear().await.or_reject()?;
         Ok(reply::with_status(reply::reply(), StatusCode::OK))
     }
 
+    /// Returns the help queue in order.
     async fn get_help_queue(help_queue: Arc<HelpQueue>) -> Result<impl Reply, Rejection> {
         let queue: Vec<u16> = help_queue.sorted().or_reject()?.collect();
         Ok(reply::with_status(reply::json(&queue), StatusCode::OK))
